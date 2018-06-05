0ede93e7f0caf4afa8cf9e8e5bd0cd8938a6035a
luismp87 <luismp87@gmail.com>
2018-06-05 09:48:39 -0500

    ss
    ss

---
+++ js/API/capture.js
@@ -0,0 +1,17 @@
+var capture = {
+    success: function(mf){
+                            var i, path, len;
+                            for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+                                path = mediaFiles[i].fullPath;
+                                // do something interesting with the file
+                            }
+        //$('#regFoto').attr('data-foto',path);
+        //$('#regFoto').html('<img src="'+path+'" style="width:100%;">');
+    },
+    captureError: function(error) {
+        navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+    },
+    takePhoto: function(){
+       navigator.device.capture.captureImage(captureSuccess, captureError, {limit:2});
+    }
+}
